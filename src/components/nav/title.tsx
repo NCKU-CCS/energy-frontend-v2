ab3c3b56da8cd0cd4839bb3b4f4216af58b2b676
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2020-07-13 20:49:44 +0800

    feat: feat i18n setting on navbar

--- src/components/nav/title.tsx
+++ src/components/nav/title.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const Title: React.FC = () => (
-  <div className="navbar-title">
-    <span>能源交易平台</span>
-  </div>
-);
+const Title: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="navbar-title">
+      <span>{t('navbar.title')}</span>
+    </div>
+  );
+};
 
 export default Title;
