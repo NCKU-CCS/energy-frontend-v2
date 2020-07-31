03d70134e797dbd1bc0565ce3189bed66809f8f2
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-07-31 17:36:22 +0800

    install d3

--- src/components/biddingPage/chart.tsx
+++ src/components/biddingPage/chart.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-/*  import data from './test.json'; */
+/* import * as d3 from 'd3'; */
+/* import data from './test.json'; */
+
+interface IProps {}
 
 const Chart: React.FC = () => {
   return <div>Chart</div>;
