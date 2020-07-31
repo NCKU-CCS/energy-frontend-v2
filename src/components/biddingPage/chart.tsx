87f97fe898562015dc2e15a3a78e295ac57daac9
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-07-31 15:37:17 +0800

    bidding page initial framework done and first git cz try

---
+++ src/components/biddingPage/chart.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Chart: React.FC = () => {
+  return <div>Chart</div>;
+};
+
+export default Chart;
---
+++ src/components/biddingPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Chart from './chart';
+
+const BiddingPageContainer: React.FC = () => (
+  <div>
+    BiddingPageContainer
+    <Chart />
+  </div>
+);
+
+export default BiddingPageContainer;
