6f45c30b67070789e026bde7dc62a9e7fc470b65
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-07-31 15:44:23 +0800

    2nd git cz try

--- src/components/biddingPage/chart.tsx
+++ src/components/biddingPage/chart.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+/*  import data from './test.json'; */
 
 const Chart: React.FC = () => {
   return <div>Chart</div>;
--- src/pages/bidding.tsx
+++ src/pages/bidding.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Layout from '../layouts/generalLayout';
+import Container from '../components/biddingPage';
 
-const BiddingPage: React.FC = () => <Layout>BIDDING</Layout>;
+const BiddingPage: React.FC = () => {
+  return (
+    <Layout>
+      BIDDING
+      <Container />
+    </Layout>
+  );
+};
 
 export default BiddingPage;
