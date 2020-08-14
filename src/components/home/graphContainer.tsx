9a286753d15351049279aa18a362e7675c77f09b
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2020-08-14 17:02:11 +0800

    feat: complete homepage structure
    Complete homepage structure with RWD setting, including time info. block, bidding info. block and
    graph container.

---
+++ src/components/home/graphContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classnames from 'classnames';
+
+const GraphContainer: React.FC = () => {
+  return (
+    <div className={classnames('home-graph-container')}>Graph container</div>
+  );
+};
+
+export default GraphContainer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Layout from '../layouts/generalLayout';
+// components
+import Home from '../components/home';
 
-const IndexPage: React.FC = () => <Layout title="首頁">INDEX</Layout>;
+const IndexPage: React.FC = () => (
+  <Layout title="首頁">
+    <Home />
+  </Layout>
+);
 
 export default IndexPage;
