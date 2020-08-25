4a658037a8fb8ef20881b21b990ecbef6012db6f
K_Jay <k20002216408@gmail.com>
2020-08-25 15:51:57 +0800

    feat: finish 1920 version
    finish 1920 version but still not connect API

--- src/components/home/bidInfo.tsx
+++ src/components/home/bidInfo.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import classnames from 'classnames';
+import data from './bidInfoTest.json';
 
 const BidInfo: React.FC = () => {
-  return <div className={classnames('home-bid-info')}>Bidding info.</div>;
+  const listItem = data.map((content) => {
+    return (
+      <div className={classnames('home-bid-info-listContainer')}>
+        <div className={classnames('home-bid-info-listContent-date')}>
+          {content.date}
+        </div>
+        <div className={classnames('home-bid-info-listContent-time')}>
+          {content.time}
+        </div>
+        <div className={classnames('home-bid-info-listContent-success')}>
+          {content.success}
+        </div>
+        <div className={classnames('home-bid-info-listContent-number')}>
+          得標度數 :{content.number}kWh
+        </div>
+        <div className={classnames('home-bid-info-listContent-price')}>
+          單價 :${content.price}/kWh
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className={classnames('home-bid-info')}>
+      <div className={classnames('home-bid-info-title')}>得標資訊</div>
+      <div className={classnames('home-bid-info-list')}>{listItem}</div>
+    </div>
+  );
 };
 
 export default BidInfo;
--- src/components/home/timeInfo.tsx
+++ src/components/home/timeInfo.tsx
@@ -2,7 +2,29 @@ import React from 'react';
 import classnames from 'classnames';
 
 const TimeInfo: React.FC = () => {
-  return <div className={classnames('home-time-info')}>Time info.</div>;
+  return (
+    <div className={classnames('home-time-info')}>
+      <div className={classnames('home-time-info-title')}>目前電力交易時間</div>
+      <div className={classnames('home-time-info-timeContainer')}>
+        <img
+          className={classnames('home-time-info-timeIcon')}
+          alt=""
+          src={`${process.env.PUBLIC_URL}/home/Green_b_icon.png`}
+        />
+        <div className={classnames('home-time-info-timeContent')}>
+          13:00 - 14:00
+        </div>
+      </div>
+      <div className={classnames('home-time-info-priceContainer')}>
+        <div className={classnames('home-time-info-buy')}>買</div>
+        <div className={classnames('home-time-info-content')}>-kWh</div>
+        <div className={classnames('home-time-info-content')}>$-/kWh</div>
+        <div className={classnames('home-time-info-sell')}>賣</div>
+        <div className={classnames('home-time-info-content')}>10kWh</div>
+        <div className={classnames('home-time-info-content')}>$5/kWh</div>
+      </div>
+    </div>
+  );
 };
 
 export default TimeInfo;
