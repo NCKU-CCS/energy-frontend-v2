43c15bfea319f309ecce91930fe0d8b58309b0f6
K_Jay <k20002216408@gmail.com>
2020-08-27 17:58:04 +0800

    feat: rWD
    finish RWD under 720 but have some problems in different height and 320 width show info

--- src/components/home/bidInfo.tsx
+++ src/components/home/bidInfo.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import data from './bidInfoTest.json';
 
 const BidInfo: React.FC = () => {
+  const [boxState, setbox] = useState<boolean>(false);
+  const [icon, seticon] = useState<string>('▶');
   const listItem = data.map((content) => {
+    const showInfo = () => {
+      if (boxState === false) setbox(true);
+      else setbox(false);
+      if (boxState === false) seticon('▼');
+      else seticon('▶');
+    };
+
     return (
       <div className={classnames('home-bid-info-listContainer')}>
+        <button
+          type="button"
+          className={classnames('home-bid-info-listContent-button')}
+          onClick={showInfo}
+        >
+          {icon}
+        </button>
         <div className={classnames('home-bid-info-listContent-date')}>
           {content.date}
         </div>
@@ -21,6 +37,20 @@ const BidInfo: React.FC = () => {
         <div className={classnames('home-bid-info-listContent-price')}>
           單價:${content.price}/kWh
         </div>
+        {boxState && (
+          <div
+            className={classnames('home-bid-info-listContent-number-nextline')}
+          >
+            得標度數:{content.number}kWh
+          </div>
+        )}
+        {boxState && (
+          <div
+            className={classnames('home-bid-info-listContent-price-nextline')}
+          >
+            單價:${content.price}/kWh
+          </div>
+        )}
       </div>
     );
   });
