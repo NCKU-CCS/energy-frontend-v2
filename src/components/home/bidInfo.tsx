d0b6a8ecbfa7102dc0b96389d43ea3b3ee318ef5
K_Jay <k20002216408@gmail.com>
2020-08-28 00:49:27 +0800

    feat: add i18n
    add i18n and fix css

--- src/components/home/bidInfo.tsx
+++ src/components/home/bidInfo.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
+import { useTranslation } from 'react-i18next';
 import data from './bidInfoTest.json';
 
 const BidInfo: React.FC = () => {
+  const { t } = useTranslation();
+
   const [boxState, setbox] = useState<boolean>(false);
   const [icon, seticon] = useState<string>('▶');
   const listItem = data.map((content) => {
@@ -12,6 +15,10 @@ const BidInfo: React.FC = () => {
       if (boxState === false) seticon('▼');
       else seticon('▶');
     };
+
+    let sellState = '';
+    if (content.success === '得標成功') sellState = t('indexpage.sellSuccess');
+    else sellState = t('indexpage.sellFail');
 
     return (
       <div className={classnames('home-bid-info-listContainer')}>
@@ -29,26 +36,26 @@ const BidInfo: React.FC = () => {
           {content.time}
         </div>
         <div className={classnames('home-bid-info-listContent-success')}>
-          {content.success}
+          {sellState}
         </div>
         <div className={classnames('home-bid-info-listContent-number')}>
-          得標度數:{content.number}kWh
+          {t('indexpage.biddingNumber')}:{content.number}kWh
         </div>
         <div className={classnames('home-bid-info-listContent-price')}>
-          單價:${content.price}/kWh
+          {t('indexpage.biddingPrice')}:${content.price}/kWh
         </div>
         {boxState && (
           <div
             className={classnames('home-bid-info-listContent-number-nextline')}
           >
-            得標度數:{content.number}kWh
+            {t('indexpage.biddingNumber')}:{content.number}kWh
           </div>
         )}
         {boxState && (
           <div
             className={classnames('home-bid-info-listContent-price-nextline')}
           >
-            單價:${content.price}/kWh
+            {t('indexpage.biddingPrice')}:${content.price}/kWh
           </div>
         )}
       </div>
@@ -57,7 +64,9 @@ const BidInfo: React.FC = () => {
 
   return (
     <div className={classnames('home-bid-info')}>
-      <div className={classnames('home-bid-info-title')}>得標資訊</div>
+      <div className={classnames('home-bid-info-title')}>
+        {t('indexpage.biddingInfoTitle')}
+      </div>
       <div className={classnames('home-bid-info-list')}>{listItem}</div>
     </div>
   );
--- src/components/home/timeInfo.tsx
+++ src/components/home/timeInfo.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import classnames from 'classnames';
+import { useTranslation } from 'react-i18next';
 
 const TimeInfo: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={classnames('home-time-info')}>
-      <div className={classnames('home-time-info-title')}>目前電力交易時間</div>
+      <div className={classnames('home-time-info-title')}>
+        {t('indexpage.tradingTime')}
+      </div>
       <div className={classnames('home-time-info-timeContainer')}>
         <img
           className={classnames('home-time-info-timeIcon')}
@@ -16,10 +21,14 @@ const TimeInfo: React.FC = () => {
         </div>
       </div>
       <div className={classnames('home-time-info-priceContainer')}>
-        <div className={classnames('home-time-info-buy')}>買</div>
+        <div className={classnames('home-time-info-buy')}>
+          {t('indexpage.buy')}
+        </div>
         <div className={classnames('home-time-info-content')}>-kWh</div>
         <div className={classnames('home-time-info-content')}>$-/kWh</div>
-        <div className={classnames('home-time-info-sell')}>賣</div>
+        <div className={classnames('home-time-info-sell')}>
+          {t('indexpage.sell')}
+        </div>
         <div className={classnames('home-time-info-content')}>10kWh</div>
         <div className={classnames('home-time-info-content')}>$5/kWh</div>
       </div>
