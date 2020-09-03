50f869bf1c6dd77cf92dc4c04e813753679bb34a
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-09-03 17:30:57 +0800

    bidding page prototype

---
+++ src/components/biddingPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const BiddingPageContainer: React.FC = () => {
+  return (
+    <div className={classNames('bidding-container')}>
+      Bidding Page Container
+    </div>
+  );
+};
+
+export default BiddingPageContainer;
