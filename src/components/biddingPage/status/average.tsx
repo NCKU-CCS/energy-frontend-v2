9118e3126cc224de86f04b3faf7a009afc511433
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-09-04 21:03:52 +0800

    add some class names

--- src/components/biddingPage/status/average.tsx
+++ src/components/biddingPage/status/average.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 interface IProps {
   averagePrice: number;
@@ -7,14 +8,21 @@ interface IProps {
 
 const Average: React.FC<IProps> = ({ averagePrice, averageVolume }) => {
   return (
-    <div>
-      <div>
-        平均交易價:
-        {averagePrice}
+    <div className={classNames('bidding-status-average-container-in')}>
+      <div className={classNames('bidding-status-average-image-container')}>
+        <img
+          className={classNames('bidding-status-average-image-img')}
+          alt=""
+          src={`${process.env.PUBLIC_URL}/biddingPage/arrow.png`}
+        />
       </div>
       <div>
-        平均交易量:
-        {averageVolume}
+        <div>${averagePrice}/kWh</div>
+        <div>平均交易價</div>
+      </div>
+      <div>
+        <div>{averageVolume}kWh</div>
+        <div>平均交易量</div>
       </div>
     </div>
   );
--- src/components/biddingPage/status/participants.tsx
+++ src/components/biddingPage/status/participants.tsx
@@ -8,11 +8,15 @@ interface IProps {
 const Participants: React.FC<IProps> = ({ participants }) => {
   return (
     <div className={classNames('bidding-status-participants-container-in')}>
-      <img
-        className={classNames('bidding-status-participants-img')}
-        alt=""
-        src={`${process.env.PUBLIC_URL}/biddingPage/people.png`}
-      />
+      <div
+        className={classNames('bidding-status-participants-image-container')}
+      >
+        <img
+          className={classNames('bidding-status-participants-image-img')}
+          alt=""
+          src={`${process.env.PUBLIC_URL}/biddingPage/people.png`}
+        />
+      </div>
       <div className={classNames('bidding-status-participants-text-container')}>
         <div className={classNames('bidding-status-participants-text-value')}>
           {participants}
