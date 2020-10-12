0c390c576e4883b2cab6a814883dda5e1fdebdc2
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-10-12 15:34:50 +0800

    feat: change mode fo infobox and some btn's pic

--- src/components/biddingPage/submit/infoBox.tsx
+++ src/components/biddingPage/submit/infoBox.tsx
@@ -2,7 +2,28 @@ import React, { useState } from 'react';
 import classNames from 'classnames';
 
 const InfoBox: React.FC = () => {
-  const [openInfoBox, setOpenInfoBox] = useState(false);
+  const [openInfoBox, setOpenInfoBox] = useState<boolean>(false);
+  const [edit, setEdit] = useState<boolean>(false);
+
+  // handle click left button
+  const handleClickLeft = () => {
+    if (edit) {
+      setEdit(false);
+      // console.log('edit : ', edit);
+    } else {
+      setEdit(true);
+      // console.log('edit : ', edit);
+    }
+  };
+
+  // handle click right
+  // const handleClickRight = () => {
+  //   if(edit) {
+
+  //   } else {
+
+  //   }
+  // };
 
   return (
     <div className={classNames('bidding-submit-infobox-container-in')}>
@@ -19,7 +40,67 @@ const InfoBox: React.FC = () => {
           />
         </button>
       ) : (
-        <div>j</div>
+        <div className={classNames('bidding-submit-infobox-canvas')}>
+          <div className={classNames('bidding-submit-infobox-content')}>
+            <div
+              className={classNames('bidding-submit-infobox-content-header')}
+            >
+              <button
+                type="button"
+                className={classNames(
+                  'bidding-submit-infobox-content-header-close',
+                )}
+                onClick={() => setOpenInfoBox(false)}
+              >
+                X
+              </button>
+            </div>
+            <div
+              className={classNames('bidding-submit-infobox-content-center')}
+            >
+              {!edit ? <div>show</div> : <div>edit</div>}
+            </div>
+            <div
+              className={classNames('bidding-submit-infobox-content-footer')}
+            >
+              <button
+                type="button"
+                className={classNames(
+                  'bidding-submit-infobox-content-footer-left',
+                )}
+                onClick={() => handleClickLeft()}
+              >
+                <img
+                  alt="left"
+                  className={classNames(
+                    'bidding-submit-infobox-content-footer-left-img',
+                  )}
+                  src={`${process.env.PUBLIC_URL}/biddingPage/${
+                    !edit ? 'edit' : 'check'
+                  }-white.png`}
+                />
+                {!edit ? '編輯' : '確認'}
+              </button>
+              <button
+                type="button"
+                className={classNames(
+                  'bidding-submit-infobox-content-footer-right',
+                )}
+              >
+                <img
+                  alt="right"
+                  className={classNames(
+                    'bidding-submit-infobox-content-footer-right-img',
+                  )}
+                  src={`${process.env.PUBLIC_URL}/biddingPage/${
+                    !edit ? 'trash' : 'cancel'
+                  }-white.png`}
+                />
+                {!edit ? '移除' : '取消'}
+              </button>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
