9ea6695519cea3380851e4a362dffc78c9d4498f
curry0622 <kingkazma0112305@gmail.com>
2020-10-19 20:39:50 +0800

    Che ping/powerInfoPage (#7)
    * bidding page initial framework done and first git cz try
    
    * 2nd git cz try
    
    * 3rd git cz try
    
    * install d3
    
    * insert a chart onto bidding page but still can't re-render after data
    changed
    
    * chart will re-render after data changed
    
    * PowerInfoPage chart done
    
    * chart in PowerInfoPage can automatically get the first date and the last
    date right now
    
    * BiddingPage is now able to change charts
    
    * PowerInfoPage is now able to change charts and data
    
    * little changes
    
    * PowerInfoPage: Basic tooltip on charts
    
    * bugs fixed on tooltip
    problem: can't add circle at correct position on curved line charts
    
    * changes. Lines starting
    :q
    
    * circle on charts position fixed
    
    * PowerInfo Page's charts prototype done
    
    * curved line is far from true data problem resolved (PowerInfo)
    
    * BiddingPage tooltip done
    
    * PowerInfo and Bidding pages' setting mode buttons are now isolated from
    chart(component)
    
    * TestApi and datePicker
    
    * change name: calendar -> datePicker
    
    * date picker with text: '2018/08/11 - 2018/08/17'
    
    * PowerInfo chart's data is now based on API data
    Problem:
    (1) If user pick a date after today, it can't fatch api data, need some
    restrictions to prevent this situation
    (2) the 0 tick of equipment mode's chart has some problems, still can't
    figure out why
    
    * chart's bugs fixed
    
    * summary(mode:淨負載)done.
    
    * summary(mode: 產能設備)done.
    
    * table can now get data from api
    
    * some css
    
    * some css
    
    * some css
    
    * table page control without css now
    
    * powerinfo page's chart can resize when window is resized
    but still got lots of problems -> using useState in useEffect can cause
    infinite loops
    
    * powerinfo page's problems resolved
    
    * call it a day
    
    * RWD of powerinfo page
    
    * infoBox at 320 in powerinfo page and RWD
    
    * add summary list component
    
    * little fix
    
    * summary-barchart done.
    
    * powerinfo page done.
    ready for MR
    
    * fix: d3 version problem & d3 scales returning number|undefined
    
    * refactor: remove IPadding(useless) in chart.tsx
    
    * refactor: move useState code to the top
    
    * fix: replace fistDate by dayjs(lastDate).subtract(6, 'day')
    
    * fix: remove changes on biddingPage in this PR
    
    * feat: datePicker can show the default value(today) now

---
+++ src/components/powerInfoPage/infoBox.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+interface IProps {
+  date: string;
+  type: string;
+  time: string;
+  value: number;
+  url: string;
+}
+
+const InfoBox: React.FC<IProps> = ({ date, type, time, value, url }) => {
+  const [infoBoxShow, setInfoBoxShow] = useState(false);
+
+  const dataType = (str: string) => {
+    switch (str) {
+      case 'Consume':
+        return '正常用電';
+      case 'NetLoad':
+        return '總淨負載';
+      case 'ESS':
+        return '儲能系統';
+      case 'EV':
+        return '充電樁';
+      case 'WT':
+        return '風能';
+      case 'PV':
+        return '太陽能';
+      default:
+        return 'error';
+    }
+  };
+
+  return (
+    <div className={classNames('powerinfo-table-infobox-container')}>
+      <button
+        type="button"
+        className={classNames('powerinfo-table-infobox-openbtn')}
+        onClick={() => setInfoBoxShow(true)}
+      >
+        查看
+      </button>
+      {infoBoxShow && (
+        <div className={classNames('powerinfo-table-infobox-canvas')}>
+          <div className={classNames('powerinfo-table-infobox-content')}>
+            <button
+              type="button"
+              className={classNames('powerinfo-table-infobox-close')}
+              onClick={() => setInfoBoxShow(false)}
+            >
+              &times;
+            </button>
+            <div className={classNames('powerinfo-table-infobox-date')}>
+              日期 :&nbsp;
+              {date}
+            </div>
+            <div className={classNames('powerinfo-table-infobox-type')}>
+              用產電種類 :&nbsp;
+              <span
+                style={value >= 0 ? { color: '#d32f2f' } : { color: '#2e7d32' }}
+              >
+                {dataType(type)}
+              </span>
+            </div>
+            <div className={classNames('powerinfo-table-infobox-time')}>
+              紀錄時間 :&nbsp;
+              {time}
+            </div>
+            <div className={classNames('powerinfo-table-infobox-value')}>
+              電力(kW) :&nbsp;
+              {value}
+            </div>
+            <div className={classNames('powerinfo-table-infobox-url')}>
+              連結 :&nbsp;
+              <a href={url}>&#60;URL&#62;</a>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InfoBox;
---
+++ src/components/powerInfoPage/modeButton.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+interface IProps {
+  changeMode(mode: string): void;
+}
+
+const ModeButton: React.FC<IProps> = ({ changeMode }) => {
+  const [disabled, setDisabled] = useState(true);
+
+  const onClickLoad = () => {
+    changeMode('淨負載');
+    setDisabled(true);
+  };
+
+  const onClickEquip = () => {
+    changeMode('產能設備');
+    setDisabled(false);
+  };
+
+  return (
+    <div className={classNames('powerinfo-mode-container')}>
+      <button
+        className={classNames(
+          'powerinfo-mode-button',
+          'powerinfo-mode-button-load',
+        )}
+        type="button"
+        onClick={() => onClickLoad()}
+        disabled={disabled}
+      >
+        淨負載
+      </button>
+      <button
+        className={classNames(
+          'powerinfo-mode-button',
+          'powerinfo-mode-button-equip',
+        )}
+        type="button"
+        onClick={() => onClickEquip()}
+        disabled={!disabled}
+      >
+        產能設備
+      </button>
+    </div>
+  );
+};
+
+export default ModeButton;
