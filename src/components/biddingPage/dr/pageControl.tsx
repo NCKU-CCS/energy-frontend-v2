f7adc22b64bc785d1da36e7a992df0c674832c76
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-10-27 21:30:20 +0800

    WIT

--- src/components/biddingPage/dr/pageControl.tsx
+++ src/components/biddingPage/dr/pageControl.tsx
@@ -5,6 +5,7 @@ const PageControl: React.FC = () => {
   return (
     <div className={classNames('bidding-dr-pagecontrol-container-in')}>
       <select className={classNames('bidding-dr-pagecontrol-select')}>
+        <option value="0">Display all</option>
         <option value="5">5 rows</option>
         <option value="10">10 rows</option>
         <option value="15">15 rows</option>
@@ -29,7 +30,7 @@ const PageControl: React.FC = () => {
       >
         &#60;
       </button>
-      <div>1 / 3</div>
+      <div>1 / 1</div>
       <button
         type="button"
         title="Next Page"
