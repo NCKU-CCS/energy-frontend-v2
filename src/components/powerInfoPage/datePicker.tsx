ed5fa9617c26b760cab956a2c27e0677df62408b
curry0622 <58351529+curry0622@users.noreply.github.com>
2020-12-17 11:56:58 +0800

    feat: new datePicker on powerInfoPage

--- src/components/powerInfoPage/datePicker.tsx
+++ src/components/powerInfoPage/datePicker.tsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import classNames from 'classnames';
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 interface IProps {
-  changeDate(date: Date): void;
+  setDate(date: Date): void;
   currDate: Date;
 }
 
-const DatePicker: React.FC<IProps> = ({ changeDate, currDate }) => {
-  const onChangeDate = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // to prevent user from choosing invalid date cause a api can't fetch problem
-    const lastDate =
-      new Date(event.target.value).getTime() > new Date().getTime()
-        ? new Date()
-        : new Date(event.target.value);
-    changeDate(lastDate);
-  };
+const DatePicker: React.FC<IProps> = ({ setDate, currDate }) => {
+  // const onChangeDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   // to prevent user from choosing invalid date cause a api can't fetch problem
+  //   const lastDate =
+  //     new Date(event.target.value).getTime() > new Date().getTime()
+  //       ? new Date()
+  //       : new Date(event.target.value);
+  //   setDate(lastDate);
+  // };
 
   return (
     <div className={classNames('powerinfo-datepicker-container')}>
-      <input
+      {/* <input
         className={classNames('powerinfo-datepicker-input')}
         type="date"
         value={dayjs(currDate).format('YYYY-MM-DD')}
         onChange={(event) => onChangeDate(event)}
-      />
+      /> */}
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          value={currDate}
+          onChange={() => setDate(new Date())}
+          format="yyyy/MM/dd"
+          disableFuture
+          allowKeyboardControl
+        />
+      </MuiPickersUtilsProvider>
     </div>
   );
 };
