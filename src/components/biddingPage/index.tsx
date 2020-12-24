a8eb238127c31c26647a91bdad6af583dd38d32a
curry0622 <kingkazma0112305@gmail.com>
2020-12-24 16:31:09 +0800

    ChePing/fixDatePickerLayout (#29)
    * fix: datepicker layout doesn't fit UI/UX designer's preference
    
    * refactor

--- src/components/biddingPage/index.tsx
+++ src/components/biddingPage/index.tsx
@@ -21,6 +21,7 @@ const BiddingPageContainer: React.FC = () => {
   return (
     <div className={classNames('bidding-container')}>
       <div className={classNames('bidding-a1')}>
+        <ModeButton setMode={setMode} />
         {mode === '需量反應' && (
           <div className={classNames('bidding-dr-date')}>
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -37,7 +38,6 @@ const BiddingPageContainer: React.FC = () => {
             </MuiPickersUtilsProvider>
           </div>
         )}
-        <ModeButton setMode={setMode} />
       </div>
       <div className={classNames('bidding-a2')}>
         <div className={classNames('bidding-a2-b1')}>
