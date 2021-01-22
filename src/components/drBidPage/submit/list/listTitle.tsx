6187e19f3caa6aa639d13dd8e6f2c8f8e636f7f6
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-01-23 01:22:05 +0800

    feat: create a directory for list

---
+++ src/components/drBidPage/submit/list/listTitle.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+import { useTranslation } from 'react-i18next';
+
+const ListTitle: React.FC = () => {
+  // i18n
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames('drbid-submit-list-title-container')}>
+      <div className={classNames('drbid-submit-list-title-date')}>
+        {t('drbidpage.date')}
+      </div>
+      <div className={classNames('drbid-submit-list-title-mode')}>
+        {t('drbidpage.mode')}
+      </div>
+      <div className={classNames('drbid-submit-list-title-volume')}>
+        {t('drbidpage.volume')}
+      </div>
+      <div className={classNames('drbid-submit-list-title-price')}>
+        {t('drbidpage.price')}
+      </div>
+      <div className={classNames('drbid-submit-list-title-totalprice')}>
+        {t('drbidpage.total')}
+      </div>
+      <div className={classNames('drbid-submit-list-title-button')}>
+        {/* <AddBidBtn type="" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default ListTitle;
