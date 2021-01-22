8647d05bb153f7d590dfe9331752dd205007d305
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-01-23 01:22:05 +0800

    feat: create a directory for list

--- src/components/drBidPage/submit/index.tsx
+++ src/components/drBidPage/submit/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import TypeButton from './typeButton';
-import List from './list';
+import List from './list/index';
 import AddBid from './addBid';
 
 interface IData {
